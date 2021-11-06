73a94e3dbedfc8fb4139cc935dafa79630cff363
Z-DIAG PC <ziyad.dev@gmail.com>
2021-11-06 11:09:28 +0100

    TouchGFX project init

---
+++ GFX01M1_HelloWorld/TouchGFX/generated/fonts/src/Kerning_arial_24_4bpp.cpp
@@ -0,0 +1,9 @@
+#include <touchgfx/Font.hpp>
+
+FONT_KERNING_LOCATION_FLASH_PRAGMA
+KEEP extern const touchgfx::KerningNode kerning_arial_24_4bpp[] FONT_KERNING_LOCATION_FLASH_ATTRIBUTE = {
+    { 0x0056, -1 },  // (First char = [0x0056, V], Second char = [0x002D, -], Kerning dist = -1)
+    { 0x0031, -2 },  // (First char = [0x0031, 1], Second char = [0x0031, 1], Kerning dist = -2)
+    { 0x0056, -1 },  // (First char = [0x0056, V], Second char = [0x003A, :], Kerning dist = -1)
+    { 0x0056, -2 },  // (First char = [0x0056, V], Second char = [0x0061, a], Kerning dist = -2)
+};
