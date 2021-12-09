825750d9e7235bb96c973008999fa1a82325c094
Ziyad MBP <ziyad.dev@gmail.com>
2021-12-09 23:58:16 +0100

    IIS3DWB implementation & tests

--- F446RE_IIS3DWB_test/Core/Src/main.c
+++ F446RE_IIS3DWB_test/Core/Src/main.c
@@ -22,6 +22,8 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
+#include <string.h>
+#include <stdio.h>
 #include "iis3dwb_reg.h"
 
 /* USER CODE END Includes */
@@ -37,13 +39,23 @@
 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
+#define    BOOT_TIME        10 //ms
 
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+SPI_HandleTypeDef hspi2;
+
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
+
+static int16_t data_raw_acceleration[3];
+static int16_t data_raw_temperature;
+static float acceleration_mg[3];
+static float temperature_degC;
+static uint8_t whoamI, rst;
+static uint8_t tx_buffer[1000];
 
 /* USER CODE END PV */
 
@@ -51,7 +63,17 @@ UART_HandleTypeDef huart2;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_SPI2_Init(void);
 /* USER CODE BEGIN PFP */
+
+static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp,
+                              uint16_t len);
+static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
+                             uint16_t len);
+static void tx_com( uint8_t *tx_buffer, uint16_t len );
+static void tx_com_ziyad( uint8_t *tx_buffer, uint16_t len );
+static void platform_delay(uint32_t ms);
+static void platform_init(void);
 
 /* USER CODE END PFP */
 
@@ -89,7 +111,52 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART2_UART_Init();
+  MX_SPI2_Init();
   /* USER CODE BEGIN 2 */
+
+  HAL_UART_Transmit(&huart2, "Init ... ", 10, 100);
+
+  stmdev_ctx_t dev_ctx;
+
+  /* Initialize mems driver interface */
+  dev_ctx.write_reg = platform_write;
+  dev_ctx.read_reg = platform_read;
+  dev_ctx.handle = &hspi2; //&SENSOR_BUS;
+
+  /* Init test platform */
+  platform_init();
+
+  /* Wait sensor boot time */
+  platform_delay(BOOT_TIME);
+
+  /* Check device ID */
+  iis3dwb_device_id_get(&dev_ctx, &whoamI);
+
+  if (whoamI != IIS3DWB_ID)
+    while (1);
+
+  HAL_UART_Transmit(&huart2, whoamI, 10, 100);
+
+  /* Restore default configuration */
+  iis3dwb_reset_set(&dev_ctx, PROPERTY_ENABLE);
+
+  do {
+    iis3dwb_reset_get(&dev_ctx, &rst);
+  } while (rst);
+
+  /* Enable Block Data Update */
+  iis3dwb_block_data_update_set(&dev_ctx, PROPERTY_ENABLE);
+
+  /* Set Output Data Rate */
+  iis3dwb_xl_data_rate_set(&dev_ctx, IIS3DWB_XL_ODR_26k7Hz);
+
+  /* Set full scale */
+  iis3dwb_xl_full_scale_set(&dev_ctx, IIS3DWB_2g);
+
+  /* Configure filtering chain(No aux interface)
+   * Accelerometer low pass filter path
+   */
+  iis3dwb_xl_filt_path_on_out_set(&dev_ctx, IIS3DWB_LP_ODR_DIV_100);
 
   /* USER CODE END 2 */
 
@@ -97,10 +164,48 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  if (HAL_UART_Transmit(&huart2, "TEST ... \n\r", 8, 100) != HAL_OK)
-			  Error_Handler();
+	    uint8_t reg;
 
-	  HAL_Delay(1000);
+	    /* Read output only if new xl value is available */
+	    iis3dwb_xl_flag_data_ready_get(&dev_ctx, &reg);
+
+	    if (reg) {
+	      /* Read acceleration field data */
+	      memset(data_raw_acceleration, 0x00, 3 * sizeof(int16_t));
+
+	      iis3dwb_acceleration_raw_get(&dev_ctx, data_raw_acceleration);
+
+	      acceleration_mg[0] =
+	        iis3dwb_from_fs2g_to_mg(data_raw_acceleration[0]);
+
+	      acceleration_mg[1] =
+	        iis3dwb_from_fs2g_to_mg(data_raw_acceleration[1]);
+
+	      acceleration_mg[2] =
+	        iis3dwb_from_fs2g_to_mg(data_raw_acceleration[2]);
+
+	      sprintf((char *)tx_buffer,
+	              "Acceleration [mg]:%4.2f\t%4.2f\t%4.2f\r\n",
+	              acceleration_mg[0], acceleration_mg[1], acceleration_mg[2]);
+
+	      tx_com_ziyad(tx_buffer, strlen((char const *)tx_buffer));
+	    }
+
+	    iis3dwb_temp_flag_data_ready_get(&dev_ctx, &reg);
+
+	    if (reg) {
+	      /* Read temperature data */
+	      memset(&data_raw_temperature, 0x00, sizeof(int16_t));
+
+	      iis3dwb_temperature_raw_get(&dev_ctx, &data_raw_temperature);
+
+	      temperature_degC = iis3dwb_from_lsb_to_celsius(data_raw_temperature);
+
+	      sprintf((char *)tx_buffer,
+	              "Temperature [degC]:%6.2f\r\n", temperature_degC);
+
+	      tx_com_ziyad(tx_buffer, strlen((char const *)tx_buffer));
+	    }
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -154,6 +259,44 @@ void SystemClock_Config(void)
 }
 
 /**
+  * @brief SPI2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_SPI2_Init(void)
+{
+
+  /* USER CODE BEGIN SPI2_Init 0 */
+
+  /* USER CODE END SPI2_Init 0 */
+
+  /* USER CODE BEGIN SPI2_Init 1 */
+
+  /* USER CODE END SPI2_Init 1 */
+  /* SPI2 parameter configuration*/
+  hspi2.Instance = SPI2;
+  hspi2.Init.Mode = SPI_MODE_MASTER;
+  hspi2.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi2.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi2.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi2.Init.CLKPhase = SPI_PHASE_2EDGE;
+  hspi2.Init.NSS = SPI_NSS_SOFT;
+  hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+  hspi2.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi2.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi2.Init.CRCPolynomial = 10;
+  if (HAL_SPI_Init(&hspi2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI2_Init 2 */
+
+  /* USER CODE END SPI2_Init 2 */
+
+}
+
+/**
   * @brief USART2 Initialization Function
   * @param None
   * @retval None
@@ -202,6 +345,9 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(IIS3DWB_CS_GPIO_Port, IIS3DWB_CS_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : B1_Pin */
@@ -209,6 +355,13 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : IIS3DWB_CS_Pin */
+  GPIO_InitStruct.Pin = IIS3DWB_CS_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  HAL_GPIO_Init(IIS3DWB_CS_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pin : LD2_Pin */
   GPIO_InitStruct.Pin = LD2_Pin;
@@ -220,6 +373,107 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+
+/*
+ * @brief  Write generic device register (platform dependent)
+ *
+ * @param  handle    customizable argument. In this examples is used in
+ *                   order to select the correct sensor bus handler.
+ * @param  reg       register to write
+ * @param  bufp      pointer to data to write in register reg
+ * @param  len       number of consecutive register to write
+ *
+ */
+static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp,
+                              uint16_t len)
+{
+	if (handle == &hspi2) {
+		HAL_GPIO_WritePin(IIS3DWB_CS_GPIO_Port, IIS3DWB_CS_Pin, GPIO_PIN_RESET);
+		HAL_SPI_Transmit(handle, &reg, 1, 1000);
+		HAL_SPI_Transmit(handle, (uint8_t*) bufp, len, 1000);
+		HAL_GPIO_WritePin(IIS3DWB_CS_GPIO_Port, IIS3DWB_CS_Pin, GPIO_PIN_SET);
+	}
+	return 0;
+}
+
+/*
+ * @brief  Read generic device register (platform dependent)
+ *
+ * @param  handle    customizable argument. In this examples is used in
+ *                   order to select the correct sensor bus handler.
+ * @param  reg       register to read
+ * @param  bufp      pointer to buffer that store the data read
+ * @param  len       number of consecutive register to read
+ *
+ */
+static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
+                             uint16_t len)
+{
+	if (handle == &hspi2) {
+		/* Read command */
+		reg |= 0x80;
+		HAL_GPIO_WritePin(IIS3DWB_CS_GPIO_Port, IIS3DWB_CS_Pin, GPIO_PIN_RESET);
+		HAL_SPI_Transmit(handle, &reg, 1, 1000);
+		HAL_SPI_Receive(handle, bufp, len, 1000);
+		HAL_GPIO_WritePin(IIS3DWB_CS_GPIO_Port, IIS3DWB_CS_Pin, GPIO_PIN_SET);
+	}
+	return 0;
+}
+
+/*
+ * @brief  Send buffer to console (platform dependent)
+ *
+ * @param  tx_buffer     buffer to transmit
+ * @param  len           number of byte to send
+ *
+ */
+static void tx_com(uint8_t *tx_buffer, uint16_t len)
+{
+#ifdef STEVAL_MKI109V3
+  CDC_Transmit_FS(tx_buffer, len);
+#elif defined(SPC584B_DIS)
+  sd_lld_write(&SD2, tx_buffer, len);
+#endif
+}
+
+/*
+ * @brief  Send buffer to console (platform dependent)
+ *
+ * @param  tx_buffer     buffer to transmit
+ * @param  len           number of byte to send
+ *
+ */
+static void tx_com_ziyad(uint8_t *tx_buffer, uint16_t len)
+{
+	if (HAL_UART_Transmit(&huart2, tx_buffer, len, 100) != HAL_OK)
+		Error_Handler();
+}
+
+
+/*
+ * @brief  platform specific delay (platform dependent)
+ *
+ * @param  ms        delay in ms
+ *
+ */
+static void platform_delay(uint32_t ms)
+{
+  HAL_Delay(ms);
+}
+
+/*
+ * @brief  platform specific initialization (platform dependent)
+ */
+static void platform_init(void)
+{
+#if defined(STEVAL_MKI109V3)
+  TIM3->CCR1 = PWM_3V3;
+  TIM3->CCR2 = PWM_3V3;
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
+  HAL_Delay(1000);
+#endif
+}
 
 /* USER CODE END 4 */
 
