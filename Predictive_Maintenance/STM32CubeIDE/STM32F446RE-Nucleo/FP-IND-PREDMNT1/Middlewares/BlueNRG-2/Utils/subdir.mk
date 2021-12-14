68bc835ae71aa2511cbd14995eb39d25d5d5f80b
Ziyad MBP <ziyad.dev@gmail.com>
2021-12-14 09:30:13 +0100

    mk files

--- Predictive_Maintenance/STM32CubeIDE/STM32F446RE-Nucleo/FP-IND-PREDMNT1/Middlewares/BlueNRG-2/Utils/subdir.mk
+++ Predictive_Maintenance/STM32CubeIDE/STM32F446RE-Nucleo/FP-IND-PREDMNT1/Middlewares/BlueNRG-2/Utils/subdir.mk
@@ -5,7 +5,7 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-C:/Users/akchoucz/STM32CubeIDE/workspace_1.7.0/Predictive_Maintenance/Middlewares/ST/BlueNRG-2/utils/ble_list.c 
+/Users/ziyad/STM32CubeIDE/workspace_1.7.0/Predictive_Maintenance/Middlewares/ST/BlueNRG-2/utils/ble_list.c 
 
 OBJS += \
 ./Middlewares/BlueNRG-2/Utils/ble_list.o 
@@ -15,7 +15,7 @@ C_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Middlewares/BlueNRG-2/Utils/ble_list.o: C:/Users/akchoucz/STM32CubeIDE/workspace_1.7.0/Predictive_Maintenance/Middlewares/ST/BlueNRG-2/utils/ble_list.c Middlewares/BlueNRG-2/Utils/subdir.mk
+Middlewares/BlueNRG-2/Utils/ble_list.o: /Users/ziyad/STM32CubeIDE/workspace_1.7.0/Predictive_Maintenance/Middlewares/ST/BlueNRG-2/utils/ble_list.c Middlewares/BlueNRG-2/Utils/subdir.mk
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DSTM32F446xx -DUSE_STM32F4XX_NUCLEO -D_DEBUG_ -D__FPU_PRESENT=1U -DARM_MATH_CM4 -DUSE_IKS01A3 -c -I../../../Inc -I../../../Patch -I../../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../../Drivers/CMSIS/Include -I../../../Drivers/CMSIS/DSP/Include -I../../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../../Drivers/BSP/IKS01A3 -I../../../Drivers/BSP/Components/Common -I../../../Drivers/BSP/Components/lps22hh -I../../../Drivers/BSP/Components/lis2mdl -I../../../Drivers/BSP/Components/lsm6dso -I../../../Middlewares/ST/STM32_MetaDataManager -I../../../Middlewares/ST/STM32_BLE_Manager/Inc -I../../../Middlewares/Third_Party/parson -I../../../Middlewares/ST/BlueNRG-2/hci/hci_tl_patterns/Basic -I../../../Middlewares/ST/STM32_MotionSP_Library/Inc -I../../../Middlewares/ST/BlueNRG-2/includes -I../../../Drivers/BSP/Components/ism330dlc -I../../../Drivers/BSP/CCA02M2 -I../../../Drivers/BSP/Components/hts221 -I../../../Middlewares/ST/BlueNRG-2/utils -I../../../Middlewares/ST/STM32_Audio/Addons/PDM/Inc -Os -ffunction-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 
 clean: clean-Middlewares-2f-BlueNRG-2d-2-2f-Utils
